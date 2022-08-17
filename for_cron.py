f2a89b32bcbc9449d4450192dda428fb64bbb2db
Laurent Claessens <laurent@claessens-donadello.eu>
2022-08-17 21:30:46 +0200

    (cron) Add my cron file.

---
+++ for_cron.py
@@ -0,0 +1,23 @@
+#!venv/bin/python3
+
+
+import time
+from station import Station
+
+
+def make_me_happy():
+    """Do the work."""
+    with Station() as station:
+        tasks = station.by_remaining()
+
+        ok_tasks = [tasks[0], tasks[-1], tasks[-2]]
+        for task in station:
+            print(task.remaining)
+            if task not in ok_tasks:
+                task.suspend()
+
+        time.sleep(1)
+        station.resume_all()
+
+
+make_me_happy()
