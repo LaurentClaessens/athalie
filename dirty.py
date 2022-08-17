8f21cb9c09769396bac1889b65991ebc3e19975c
Laurent Claessens <laurent@claessens-donadello.eu>
2022-08-17 19:45:57 +0200

    (tasks) Take the output of `boinccmd --get_tasks` and create a json with the tasks' properties.

--- dirty.py
+++ dirty.py
@@ -4,6 +4,28 @@
 import subprocess
 
 
-def json_tasks():
-    """Return the list of tasks as json like object."""
+dprint = print
 
+
+def pak_to_task(pak):
+    """Return a json like task from its output"""
+    task = {}
+    for line in pak.splitlines():
+        if ": " not in line:
+            continue
+        parts = line.split(":")
+        key = parts[0].strip()
+        value = parts[1].strip()
+        task[key] = value
+    return task
+
+
+def get_json_tasks():
+    """Return the list of tasks as json like object."""
+    b_output = subprocess.check_output(['boinccmd', '--get_tasks'], timeout=2)
+    text = b_output.decode('utf8')
+    paks = text.split(") -----------")
+    tasks = []
+    for pak in paks:
+        task = pak_to_task(pak)
+        tasks.append(task)
--- work.py
+++ work.py
@@ -1,9 +1,12 @@
 #!venv/bin/python3
 
 
+from dirty import get_json_tasks
+
 
 def do_work():
     """Do the work."""
+    json_tasks = get_json_tasks()
 
 
 
